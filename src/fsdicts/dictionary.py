1bc6fd99f83632926439b0c0955b20f45445a273
Nadav Tasher <tashernadav@gmail.com>
2024-02-16 22:21:44 +0200

    Some minor code improvements

--- src/fsdicts/dictionary.py
+++ src/fsdicts/dictionary.py
@@ -33,32 +33,20 @@ class Dictionary(AdvancedMutableMapping):
     def _child_instance(self, path):
         return Dictionary(path, (self._key_storage, self._value_storage), (self._encode, self._decode), self._lock)
 
-    def _internal_iter(self):
+    def _internal_iterate(self):
         # List all of the items in the path
         for checksum in os.listdir(self._path):
-            # Create key and value paths
-            key_path, value_path = os.path.join(self._path, checksum, FILE_KEY), os.path.join(self._path, checksum, FILE_VALUE)
+            # Create item path
+            item_path = os.path.join(self._path, checksum)
 
-            # Make sure the key path exists and is a valid link
-            if not self._key_storage.islink(key_path):
+            # Make sure the item is valid
+            if not self._internal_has(item_path):
                 continue
-
-            # Make sure the value path exists
-            if not os.path.exists(value_path):
-                continue
-
-            # If the value path is a file, should also be a link
-            if os.path.isfile(value_path):
-                if not self._value_storage.islink(value_path):
-                    continue
 
             # Yield the checksum
-            yield checksum, key_path
+            yield item_path
 
-    def _internal_hasitem(self, key):
-        # Resolve item path
-        item_path = self._item_path(key)
-
+    def _internal_has(self, item_path):
         # Resolve value path
         key_path, value_path = os.path.join(item_path, FILE_KEY), os.path.join(item_path, FILE_VALUE)
 
@@ -78,13 +66,13 @@ class Dictionary(AdvancedMutableMapping):
         # Check if paths exist
         return os.path.isdir(item_path)
 
-    def _internal_setitem(self, key, value):
-        # Delete the old value
-        if self._internal_hasitem(key):
-            self._internal_delitem(key)
-
+    def _internal_set(self, key, value):
         # Resolve item path
         item_path = self._item_path(key)
+
+        # Delete the old value
+        if self._internal_has(item_path):
+            self._internal_delete(key)
 
         # Create the item path
         os.makedirs(item_path)
@@ -106,13 +94,13 @@ class Dictionary(AdvancedMutableMapping):
             # Store the value in the object storage
             self._value_storage.put(self._encode(value), value_path)
 
-    def _internal_getitem(self, key):
-        # Make sure key exists
-        if not self._internal_hasitem(key):
-            raise KeyError(key)
-
+    def _internal_get(self, key):
         # Resolve item path
         item_path = self._item_path(key)
+
+        # Make sure key exists
+        if not self._internal_has(item_path):
+            raise KeyError(key)
 
         # Resolve value path
         value_path = os.path.join(item_path, FILE_VALUE)
@@ -125,13 +113,13 @@ class Dictionary(AdvancedMutableMapping):
             # Read the value, decode and return
             return self._decode(self._value_storage.readlink(value_path))
 
-    def _internal_delitem(self, key):
-        # Make sure key exists
-        if not self._internal_hasitem(key):
-            raise KeyError(key)
-
+    def _internal_delete(self, key):
         # Resolve item path
         item_path = self._item_path(key)
+
+        # Make sure key exists
+        if not self._internal_has(item_path):
+            raise KeyError(key)
 
         # Resolve value path
         key_path, value_path = os.path.join(item_path, FILE_KEY), os.path.join(item_path, FILE_VALUE)
@@ -169,7 +157,7 @@ class Dictionary(AdvancedMutableMapping):
 
         # Lock the item for modifications
         with self._lock(item_path):
-            return self._internal_getitem(key)
+            return self._internal_get(key)
 
     def __setitem__(self, key, value):
         # Resolve item path
@@ -177,7 +165,7 @@ class Dictionary(AdvancedMutableMapping):
 
         # Lock the item for modifications
         with self._lock(item_path):
-            return self._internal_setitem(key, value)
+            return self._internal_set(key, value)
 
     def __delitem__(self, key):
         # Resolve item path
@@ -185,7 +173,7 @@ class Dictionary(AdvancedMutableMapping):
 
         # Lock the item for modifications
         with self._lock(item_path):
-            return self._internal_delitem(key)
+            return self._internal_delete(key)
 
     def __contains__(self, key):
         # Resolve item path
@@ -193,21 +181,21 @@ class Dictionary(AdvancedMutableMapping):
 
         # Check if paths exist
         with self._lock(item_path):
-            return self._internal_hasitem(key)
+            return self._internal_has(item_path)
 
     def __iter__(self):
         # List all of the items in the path
-        for checksum, key_path in self._internal_iter():
+        for item_path in self._internal_iterate():
             # Read the key contents and decode
-            with self._lock(os.path.join(self._path, checksum)):
-                key_contents = self._decode(self._key_storage.readlink(key_path))
+            with self._lock(item_path):
+                key_contents = self._decode(self._key_storage.readlink(os.path.join(item_path, FILE_KEY)))
 
             # Yield the key contents
             yield key_contents
 
     def __len__(self):
         # Count all key files
-        return len(list(self._internal_iter()))
+        return len(list(self._internal_iterate()))
 
     def __repr__(self):
         # Format the data like a dictionary
@@ -238,14 +226,14 @@ class Dictionary(AdvancedMutableMapping):
             # Check if paths exist
             with self._lock(item_path):
                 # Fetch the value
-                value = self._internal_getitem(key)
+                value = self._internal_get(key)
 
                 # Check if the value is a keystore
                 if isinstance(value, Mapping):
                     value = value.copy()
 
                 # Delete the item
-                self._internal_delitem(key)
+                self._internal_delete(key)
 
             # Return the value
             return value
